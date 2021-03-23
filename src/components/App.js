ba8cd4dd0655fac53f380fbf23ae96654eb6c3f8
Conor Murnane <conormurnane11@gmail.com>
2021-03-23 18:45:46 -0400

    header complete

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,32 @@ import "bootstrap/dist/css/bootstrap.min.css"
 // import myLinkedIn from "../linkedin.jp"
 import NavBar from "./NavBar"
 import Header from "./Header"
+import Particles from "react-particles-js"
 
 function App() {
   return (
     <>
+    <Particles
+    className= "particles-canvas"
+      params={{
+        particles: {
+          number: {
+            value: 15,
+            density: {
+              enable: true,
+              value_area: 700 
+            }
+          },
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 15,
+              color: "#c2c2f0"
+            }
+          }
+        }
+      }}
+    />
     <NavBar/>
     <Header/>
     </>
