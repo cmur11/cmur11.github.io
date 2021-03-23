da37534453fc13e46f09989fc99c761c442a1098
Conor Murnane <conormurnane11@gmail.com>
2021-03-23 17:50:26 -0400

    header working

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 import React from "react"
+import Typed from "react-typed"
+
 function Header(){
 return(
   <div className = "header-wrapper">
       <div className = "main-info">
-          <h1>Web development promotions </h1>
+          <h1>Software Engineer </h1>
+          <Typed 
+            className = "typed-text"
+            strings = {["Welcome!", "My name is Conor Murnane and I am a software engineer", "Please check out my page!" ]}
+            typeSpeed={40}
+            backSpeed={50}
+            loop
+          />
+          <a href = "#" className= "btn-main-info">Contact Me</a>
       </div>
   </div>
 )
