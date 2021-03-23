bb04dc58a6f1f70b62563ddeafedf3534bd59332
Conor Murnane <conormurnane11@gmail.com>
2021-03-23 17:11:46 -0400

    everything working

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import '../App.css'
 import "bootstrap/dist/css/bootstrap.min.css"
 // import myLinkedIn from "../linkedin.jp"
 import NavBar from "./NavBar"
+import Header from "./Header"
 
 function App() {
   return (
-
+    <>
     <NavBar/>
-
+    <Header/>
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+function Header(){
+return(
+  <div className = "header-wrapper">
+      <div className = "main-info">
+          <h1>Web development promotions </h1>
+      </div>
+  </div>
+)
+}
+
+export default Header;
