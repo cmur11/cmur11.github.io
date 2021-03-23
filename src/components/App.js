e7fc5fd70b8079a16a3dd2f1a5121f35d0c00ac2
Conor Murnane <conormurnane11@gmail.com>
2021-03-23 14:19:55 -0400

    project setup

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+// import NavBar from "./NavBar.js"
+import "bootstrap/dist/css/bootstrap.min.css"
+// import {Route, BrowserRouter as Router} from "react-router-dom";
+function App() {
+  return (
+ 
+    <div className="App">
+      
+      <header className="App-header">
+        Conor
+      </header>
+    </div>
+  
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+// import { NavLink } from 'react-router-dom';
+// import { Menu } from 'semantic-ui-react'
+// function NavBar(){
+    
+//         return(
+//                  <Menu>
+//                         <Menu.Item><NavLink to= "/home/">Home</NavLink></Menu.Item>
+//                         <Menu.Item><NavLink to= "/applied_listings">Applied Apartments</NavLink></Menu.Item>
+//                         <Menu.Item><NavLink to= "/saved_listings">Saved Apartments</NavLink></Menu.Item>
+//                         <Menu.Menu position= "right">
+//                             <Menu.Item><NavLink to= "/login">Logout  </NavLink></Menu.Item>
+//                         </Menu.Menu>
+//                  </Menu>
+       
+//         )
+// }
+// export default NavBar;
