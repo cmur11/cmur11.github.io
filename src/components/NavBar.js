bcf77f4d767bb1ae503e096c5150eeb5aee9ae7b
Conor Murnane <conormurnane11@gmail.com>
2021-03-23 16:35:14 -0400

    working

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,7 +12,7 @@ function NavBar(){
               <a className="navbar-brand" href="#">Conor Murnane</a>
               {/* <img src = {linkedIn} alt = "Conor Photo" className = "logo"/> */}
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <FontAwesomeIcon icon={faBars} style={{color: "fff"}}/>
+                <FontAwesomeIcon icon={faBars} style={{color: "#fff"}}/>
                 {/* creates menu hamburger */}
               </button>
               <div className="collapse navbar-collapse" id="navbarSupportedContent">
